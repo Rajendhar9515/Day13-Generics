e4d5720bee634f4932a12fc11bc2c9f7cbb5f76f
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-28 17:02:46 +0530

    All 3 in one generic method is solved.

--- Find_Maximum.java
+++ Find_Maximum.java
@@ -1,55 +1,22 @@
 package com.bridgelabz.generics;
 
-// Generic class
-public class Find_Maximum <T extends Comparable<T>>{
-	
-	// Constant variables 
-	private final T VAR1;
-	private final T VAR2;
-	private final T VAR3;
-	private final T VAR4;
-	private final T VAR5;
-		
-	// Constructor 
-	public Find_Maximum(T var1, T var2, T var3, T var4, T var5) {
-		this.VAR1 = var1;
-		this.VAR2 = var2;
-		this.VAR3 = var3;
-		this.VAR4 = var4;
-		this.VAR5 = var5;
-	}
-	
-	public T testMaximum() {
-		T findMax = Find_Maximum.testMaximum(VAR1, VAR2, VAR3, VAR4, VAR5);
-		printMax(findMax); //calling printMaximum method.
-		return findMax;
-	}
-	
-	// Creating print maximum function 
-	public void printMax(T max) {
-		System.out.println("\nMAXIMUM IS :- " + max);
-	} 
+public class Find_Maximum {
 	
 	// Method by using generic
-	public static <T extends Comparable <T>> T testMaximum(T var1 , T var2 , T var3 , T var4 , T var5) {
-		
-		T max = var5;
+	public <T extends Comparable<T>> T findMax(T var1, T var2, T var3) {
+
+		T max = var3;
+
 		// comparing numbers using compareTo method.
-	    if(var1.compareTo(var2)>0 && var1.compareTo(var3)>0 && var1.compareTo(var4)>0 && var1.compareTo(max)>0) {
-	    	max = var1;
-	    }
-	    else if(var2.compareTo(var1)>0 && var2.compareTo(var3)>0 && var2.compareTo(var4)>0 && var2.compareTo(max)>0) {
-	        max = var2;
-	    }
-	    else if(var3.compareTo(var1)>0 && var3.compareTo(var2)>0 && var3.compareTo(var4)>0 && var3.compareTo(max)>0){
-	    	max = var3;
-	    }
-	    else if(var4.compareTo(var1)>0 && var4.compareTo(var2)>0 && var4.compareTo(var3)>0 && var4.compareTo(max)>0) {
-	    	max = var4;
-	    }
-	    else {
-	    	max = var5;
-	    }
-	    return max;
+		if (var1.compareTo(var2) > 0 && var1.compareTo(max) > 0) {
+			max = var1;
+			System.out.println("\n\nTC-1.1 :- " + max + " is maximum.");
+		} else if (var2.compareTo(var1) > 0 && var2.compareTo(max) > 0) {
+			max = var2;
+			System.out.println("\nTC-1.2 :- " + max + " is maximum.");
+		} else {
+			System.out.println("\nTC-1.3 :- " + max + " is maximum.");
+		}
+		return max;
 	}
 }
--- Main_Find_Maximum.java
+++ Main_Find_Maximum.java
@@ -1,34 +1,28 @@
 package com.bridgelabz.generics;
 
 public class Main_Find_Maximum {
-	//main method
+	
+	// main method
 	public static void main(String[] args) {
 		
-		// Displayed welcome message 
-	    System.out.println("\t FIND MAXIMUM USING GENERIC \n");
-	    
-	    // Test Cases for integers.
-	    Find_Maximum<Integer> ITC1 = new Find_Maximum<Integer>(45 , 38 , 24 , 15 , 42); //TC-1.1 :- max num at first position.
-	    ITC1.testMaximum();
-	    Find_Maximum<Integer> ITC2 = new Find_Maximum<Integer>(18, 28, 19 , 10 , 12);  //TC-1.2 :- max num at second position.
-	    ITC2.testMaximum();
-	    Find_Maximum<Integer> ITC3 = new Find_Maximum<Integer>(75, 52, 120 , 80 , 117); //TC-1.3 :- max num at third position.
-	    ITC3.testMaximum();
-	    
-	    // Test cases for floats. 
-	    Find_Maximum<Float> FTC1 = new Find_Maximum<Float>(15.5f, 12.8f, 9.56f , 7.24f , 13.53f); 
-	    FTC1.testMaximum();
-	    Find_Maximum<Float> FTC2 = new Find_Maximum<Float>(12.4f, 25.8f, 15.7f , 17.2f , 2.84f); 
-	    FTC2.testMaximum();
-	    Find_Maximum<Float> FTC3 = new Find_Maximum<Float>(14.2f, 18.7f, 28.9f , 14.5f , 22.3f); 
-	    FTC3.testMaximum();
-	    
-	    // Test cases for Strings. 
-	    Find_Maximum<String> STC1 = new Find_Maximum<String>("Yesdhani", "Nikhil", "Ajay", "Sam", "Ram");  //TC-1.1 :- max string at first position.
-	    STC1.testMaximum();
-	    Find_Maximum<String> STC2 = new Find_Maximum<String>("car", "train", "bike", "bus", "jeep");  //TC-1.2 :- max string at second position.
-	    STC2.testMaximum();
-	    Find_Maximum<String> STC3 = new Find_Maximum<String>("bat", "ball", "wickets", "Stump", "jersey"); //TC-1.3 :- max string at third position. 
-	    STC3.testMaximum();
+		// Displayed welcome message
+		System.out.println("\t FIND MAXIMUM USING GENERICS \n");
+
+		Find_Maximum object = new Find_Maximum();
+
+		// Test Cases for integers.
+		object.findMax(45, 38, 24); // TC-1.1 :- max num at first position.
+		object.findMax(18, 28, 19); // TC-1.2 :- max num at second position.
+		object.findMax(75, 52, 120); // TC-1.3 :- max num at third position.
+
+		// Test cases for floats.
+		object.findMax(15.5f, 12.8f, 9.56f); // TC-1.1 :- max num at first position.
+		object.findMax(12.4f, 25.8f, 15.7f); // TC-1.2 :- max num at second position.
+		object.findMax(14.2f, 18.7f, 28.9f); // TC-1.3 :- max num at third position.
+
+		// Test cases for Strings.
+		object.findMax("Yesdhani", "Nikhil", "Ajay"); // TC-1.1 :- max string at first position.
+		object.findMax("car", "cycle", "bike"); // TC-1.2 :- max string at second position.
+		object.findMax("bat", "ball", "wickets"); // TC-1.3 :- max string at third position.
 	}
 }
